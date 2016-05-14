ccc437d6ff913cb8e426d3fb499b6d8b09a742f0
iMokhles <iMokhles@iMokhless-iMac.local>
2016-05-14 18:50:14 +0200

    first commit

---
+++ SysSecInfoLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WAE9Loader
+//
+//  Created by iMokhles on 13/05/16.
+//  Copyright © 2016 iMokhles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SysSecInfoLoader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WAE9Loader
+//
+//  Created by iMokhles on 13/05/16.
+//  Copyright © 2016 iMokhles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
